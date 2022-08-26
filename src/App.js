59f0e7c0c3159921b93aa00f88e59e80c681c613
jhersbo <jhersbo@gmail.com>
2022-08-26 12:25:02 -0500

    environment work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
 import './App.css';
 
+import {useState, useEffect} from 'react'
+import Cookies from 'cookies-js'
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"
+import { useMediaQuery } from '@mui/material'
+
+//server URL
+const serverURL = "http://localhost:3000/"
+
 function App() {
+
+  //state variables
+  let [thinScreen, setThinScreen] = useState(false)
+
+  //screen size state
+  const thinScreenBool = useMediaQuery('(max-width: 900px)')
+
+  //cookies for login
+  let cookieUser = Cookies.get('user')
+  if(cookieUser){
+    cookieUser = JSON.parse(cookieUser)
+  }
+
+  useEffect(()=>{
+    console.log('App.js is mounted.')
+    //screen size
+    setThinScreen(thinScreenBool)
+  })
+
   return (
     <div className="App">
       
