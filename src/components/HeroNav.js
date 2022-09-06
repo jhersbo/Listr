e90511648da5fca2230ef04f413e939f61dcd4ac
jhersbo <jhersbo@gmail.com>
2022-09-06 11:23:10 -0500

    building

--- src/components/HeroNav.js
+++ src/components/HeroNav.js
@@ -1,4 +1,6 @@
-import { Paper } from "@mui/material"
+import { Avatar, IconButton, Paper } from "@mui/material"
+import { minWidth } from "@mui/system"
+import PersonIcon from '@mui/icons-material/Person';
 
 const HeroNav = ({ thinScreen, user, setUser})=>{
     if(thinScreen){
@@ -7,9 +9,26 @@ const HeroNav = ({ thinScreen, user, setUser})=>{
                 bgcolor: "#94d2bd", 
                 minHeight: "4em",
                 display: 'flex',
-                alignContent: 'center'
+                alignContent: 'center',
+                justifyContent: "space-between"
+
             }}>
                 <h1 className="hero">Listr</h1>
+                <IconButton sx={{ 
+                    p: 1,
+                }} href='/user'>
+                    <Avatar sx={{
+                        height: '2em',
+                        width: '2em',
+                        marginTop: '0.5em',
+                        marginRight: '0.5em',
+                        boxShadow: '0px 1px 4px black'
+                    }}
+                        
+                    >
+                        {user? user.name.charAt(0): <PersonIcon></PersonIcon>}
+                    </Avatar>
+                </IconButton>
             </Paper>
         )
     }
--- src/components/ListPanel.js
+++ src/components/ListPanel.js
@@ -1,11 +1,24 @@
 import ListItem from "./ListItem"
 
-const ListPanel = ()=>{
-    return(
-        <div>
-            <h1>ListPanel</h1>
-        </div>
-    )
+const ListPanel = ({ thinScreen, user, listDB })=>{
+
+    if(!user){
+        return(
+            <h3>Sign in to manage your list.</h3>
+        )
+    }else{
+        const usersList = listDB.filter(element => element.user_id === user.user_id)
+        const individualItems = usersList.map((item, index)=>{
+            return(
+                <ListItem key={item.item_id} user={user} thinScreen={thinScreen} listDB={listDB}></ListItem>
+            )
+        })
+        return(
+            <h3>Sug</h3>
+        )
+    }
+
+
 }
 
 export default ListPanel
