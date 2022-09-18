c4826dbea8709881cd186ae24eb9d4316af6c084
jhersbo <jhersbo@gmail.com>
2022-09-18 17:29:28 -0500

    last main  version for a while

--- src/components/minor-components/SignedOutPage.js
+++ src/components/minor-components/SignedOutPage.js
@@ -1,4 +1,8 @@
-import { Paper } from "@mui/material"
+import { Paper, Button } from "@mui/material"
+
+const handleClick = ()=>{
+    window.location.href = '/user'
+}
 
 
 const SignedOutPage = ()=>{
@@ -6,9 +10,24 @@ const SignedOutPage = ()=>{
         <Paper elevation={2} sx={{
             margin: '2%',
             boxShadow: "5px 5px 20px black",
-            padding: "20% 3% 3%"
+            padding: "20% 3% 3%",
+            display: "flex",
+            flexDirection: "column"
         }}>
-            <h2>Sign in to manage your lists.</h2>
+            <h2>Sign in to manage your lists!</h2>
+            <Button variant="text" onClick={()=>{handleClick()}}sx={{
+                    color: "#94d2bd",
+                    fontFamily: "Antonio, sans-serif",
+                    // textShadow: "-1px -1px 0 #003554b, 1px -1px 0 #003554b, -1px 1px 0 #003554b, 1px 1px 0 #003554b;",
+                    fontSize: "21px",
+                    width: "50%",
+                    padding: "0%",
+                    margin: "2%",
+                    alignSelf: "center",
+                    border: "1px solid #003554",
+                    bgcolor: "#003554",
+                    boxShadow: "1px 1px 5px black"
+                }}>Sign in</Button>
         </Paper>
     )
 }
